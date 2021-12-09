6eb42a2de4f2a8b0f3401936f6b8b5b1445e2883
blobmaster <blobmaster.crux120@passmail.net>
2021-12-09 12:38:22 +0530

    decompile: upstream f9ffd57d8f829cc9079d9c51da6886844eee94d0 (2021-12-09) -- 2 files changed
    Upstream commit: https://github.com/espressif/esp32c3-bt-lib/commit/f9ffd57d8f829cc9079d9c51da6886844eee94d0
    
    Upstream subject: Fixing scenario where ACL Rx data is sent to host before enc change event [19bdc26c].
    
    Archives decompiled: 1

--- decompiled/libbtdm_app/arch_main/btdm_controller_get_compile_version.c
+++ decompiled/libbtdm_app/arch_main/btdm_controller_get_compile_version.c
@@ -1,8 +1,8 @@
 /*
- * Last changed at upstream commit 32f15e826a102d2d64e612620468122ea2234a2e
- * https://github.com/espressif/esp32c3-bt-lib/commit/32f15e826a102d2d64e612620468122ea2234a2e
- * Upstream date: 2021-09-17 17:05:04 +0800
- * Upstream subject: Update ESP32-C3 and ESP32-S3 bt lib (3e61eea)
+ * Last changed at upstream commit f9ffd57d8f829cc9079d9c51da6886844eee94d0
+ * https://github.com/espressif/esp32c3-bt-lib/commit/f9ffd57d8f829cc9079d9c51da6886844eee94d0
+ * Upstream date: 2021-12-09 12:38:22 +0530
+ * Upstream subject: Fixing scenario where ACL Rx data is sent to host before enc change event [19bdc26c].
  * Source: libbtdm_app -> arch_main.o -> btdm_controller_get_compile_version
  *
  * (C) Espressif, Apache License 2.0.
@@ -13,6 +13,6 @@
 char * btdm_controller_get_compile_version(void)
 
 {
-  return "3e61eea";
+  return "19bdc26";
 }
 
