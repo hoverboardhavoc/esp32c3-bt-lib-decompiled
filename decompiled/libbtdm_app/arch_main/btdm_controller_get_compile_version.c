a73e024a2892345ef0d9b2f8e195017d01852bce
blobmaster <blobmaster.crux120@passmail.net>
2023-04-28 14:11:07 +0800

    decompile: upstream a17e093350f31d09b8bcbfaad40b5b35535ea97b (2023-04-28) -- 2 files changed
    Upstream commit: https://github.com/espressif/esp32c3-bt-lib/commit/a17e093350f31d09b8bcbfaad40b5b35535ea97b
    
    Upstream subject: Update bt lib for ESP32-C3 and ESP32-S3(19c7783)
    
    Archives decompiled: 1

--- decompiled/libbtdm_app/arch_main/btdm_controller_get_compile_version.c
+++ decompiled/libbtdm_app/arch_main/btdm_controller_get_compile_version.c
@@ -1,8 +1,8 @@
 /*
- * Last changed at upstream commit 4f71ee7fab59e3114d0ac0144b38df973c90261e
- * https://github.com/espressif/esp32c3-bt-lib/commit/4f71ee7fab59e3114d0ac0144b38df973c90261e
- * Upstream date: 2023-04-14 16:45:28 +0800
- * Upstream subject: Update bt lib for ESP32-C3 and ESP32-S3(02de671f)
+ * Last changed at upstream commit a17e093350f31d09b8bcbfaad40b5b35535ea97b
+ * https://github.com/espressif/esp32c3-bt-lib/commit/a17e093350f31d09b8bcbfaad40b5b35535ea97b
+ * Upstream date: 2023-04-28 14:11:07 +0800
+ * Upstream subject: Update bt lib for ESP32-C3 and ESP32-S3(19c7783)
  * Source: libbtdm_app -> arch_main.o -> btdm_controller_get_compile_version
  *
  * (C) Espressif, Apache License 2.0.
@@ -13,6 +13,6 @@
 char * btdm_controller_get_compile_version(void)
 
 {
-  return "02de671";
+  return "19c7783";
 }
 
--- decompiled/libbtdm_app/lld_con/r_lld_con_start_hook.c
+++ decompiled/libbtdm_app/lld_con/r_lld_con_start_hook.c
@@ -1,8 +1,8 @@
 /*
- * Last changed at upstream commit 5c6ab5248a124cffc731a9e4764473fdeef38054
- * https://github.com/espressif/esp32c3-bt-lib/commit/5c6ab5248a124cffc731a9e4764473fdeef38054
- * Upstream date: 2023-03-09 14:58:19 +0800
- * Upstream subject: Update bt lib for ESP32-C3 and ESP32-S3(85a1090)
+ * Last changed at upstream commit a17e093350f31d09b8bcbfaad40b5b35535ea97b
+ * https://github.com/espressif/esp32c3-bt-lib/commit/a17e093350f31d09b8bcbfaad40b5b35535ea97b
+ * Upstream date: 2023-04-28 14:11:07 +0800
+ * Upstream subject: Update bt lib for ESP32-C3 and ESP32-S3(19c7783)
  * Source: libbtdm_app -> lld_con.o -> r_lld_con_start_hook
  *
  * (C) Espressif, Apache License 2.0.
@@ -17,7 +17,7 @@ undefined4 r_lld_con_start_hook(int param_1,int param_2)
 {
   int iVar1;
   
-  if (param_1 == 0) {
+  if (param_1 != 0) {
     iVar1 = (**(code **)(_r_plf_funcs_p + 0xf0))(*(code **)(_r_plf_funcs_p + 0xf0));
     if (*(char *)(iVar1 + 0x18) == '\0') {
       *(undefined1 *)(param_2 + 0x26) = 0;
