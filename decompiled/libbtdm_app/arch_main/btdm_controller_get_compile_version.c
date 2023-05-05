c3dd7bd1142227a039224e032646fd87e43102a9
blobmaster <blobmaster.crux120@passmail.net>
2023-05-05 21:49:57 +0800

    decompile: upstream d7513ebe88d7d207f6c062ce9381d347a0a70918 (2023-05-05) -- 2 files changed
    Upstream commit: https://github.com/espressif/esp32c3-bt-lib/commit/d7513ebe88d7d207f6c062ce9381d347a0a70918
    
    Upstream subject: Update bt lib for ESP32-C3 and ESP32-S3(85b425c)
    
    Archives decompiled: 1

--- decompiled/libbtdm_app/arch_main/btdm_controller_get_compile_version.c
+++ decompiled/libbtdm_app/arch_main/btdm_controller_get_compile_version.c
@@ -1,8 +1,8 @@
 /*
- * Last changed at upstream commit a17e093350f31d09b8bcbfaad40b5b35535ea97b
- * https://github.com/espressif/esp32c3-bt-lib/commit/a17e093350f31d09b8bcbfaad40b5b35535ea97b
- * Upstream date: 2023-04-28 14:11:07 +0800
- * Upstream subject: Update bt lib for ESP32-C3 and ESP32-S3(19c7783)
+ * Last changed at upstream commit d7513ebe88d7d207f6c062ce9381d347a0a70918
+ * https://github.com/espressif/esp32c3-bt-lib/commit/d7513ebe88d7d207f6c062ce9381d347a0a70918
+ * Upstream date: 2023-05-05 21:49:57 +0800
+ * Upstream subject: Update bt lib for ESP32-C3 and ESP32-S3(85b425c)
  * Source: libbtdm_app -> arch_main.o -> btdm_controller_get_compile_version
  *
  * (C) Espressif, Apache License 2.0.
@@ -13,6 +13,6 @@
 char * btdm_controller_get_compile_version(void)
 
 {
-  return "19c7783";
+  return "85b425c";
 }
 
