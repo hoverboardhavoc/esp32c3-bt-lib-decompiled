05603ae3e59c41f33c564c3271e7d7a70bf628d9
blobmaster <blobmaster.crux120@passmail.net>
2024-09-21 10:42:43 +0800

    decompile: upstream 061e70c319576bf28b5695f5478dbe01ad18b18d (2024-09-21) -- 2 files changed
    Upstream commit: https://github.com/espressif/esp32c3-bt-lib/commit/061e70c319576bf28b5695f5478dbe01ad18b18d
    
    Upstream subject: fix(bt): Update bt lib for ESP32-C3 and ESP32-S3(63eb54d)
    
    Archives decompiled: 1

--- decompiled/libbtdm_app/arch_main/btdm_controller_get_compile_version.c
+++ decompiled/libbtdm_app/arch_main/btdm_controller_get_compile_version.c
@@ -1,8 +1,8 @@
 /*
- * Last changed at upstream commit 5c913171361dac0821e15e83cc0cf604149e1b84
- * https://github.com/espressif/esp32c3-bt-lib/commit/5c913171361dac0821e15e83cc0cf604149e1b84
- * Upstream date: 2024-09-19 15:19:37 +0800
- * Upstream subject: fix(bt): Update bt lib for ESP32-C3 and ESP32-S3(e9ae856)
+ * Last changed at upstream commit 061e70c319576bf28b5695f5478dbe01ad18b18d
+ * https://github.com/espressif/esp32c3-bt-lib/commit/061e70c319576bf28b5695f5478dbe01ad18b18d
+ * Upstream date: 2024-09-21 10:42:43 +0800
+ * Upstream subject: fix(bt): Update bt lib for ESP32-C3 and ESP32-S3(63eb54d)
  * Source: libbtdm_app -> arch_main.o -> btdm_controller_get_compile_version
  *
  * (C) Espressif, Apache License 2.0.
@@ -13,6 +13,6 @@
 char * btdm_controller_get_compile_version(void)
 
 {
-  return "e9ae856";
+  return "63eb54d";
 }
 
--- decompiled/libbtdm_app/llm/llm_rpa_renew_to_handler_hack.c
+++ decompiled/libbtdm_app/llm/llm_rpa_renew_to_handler_hack.c
@@ -1,8 +1,8 @@
 /*
- * Last changed at upstream commit 9780fd66d97866977d972fb63319830f47f12785
- * https://github.com/espressif/esp32c3-bt-lib/commit/9780fd66d97866977d972fb63319830f47f12785
- * Upstream date: 2024-04-23 11:19:15 +0800
- * Upstream subject: fix(bt): Update bt lib for ESP32-C3 and ESP32-S3(a771b7c)
+ * Last changed at upstream commit 061e70c319576bf28b5695f5478dbe01ad18b18d
+ * https://github.com/espressif/esp32c3-bt-lib/commit/061e70c319576bf28b5695f5478dbe01ad18b18d
+ * Upstream date: 2024-09-21 10:42:43 +0800
+ * Upstream subject: fix(bt): Update bt lib for ESP32-C3 and ESP32-S3(63eb54d)
  * Source: libbtdm_app -> llm.o -> llm_rpa_renew_to_handler_hack
  *
  * (C) Espressif, Apache License 2.0.
@@ -22,8 +22,8 @@ undefined4 llm_rpa_renew_to_handler_hack(void)
     (**(code **)(_r_modules_funcs_p + 0xc4))
               (6,0,(uint)*(ushort *)(_p_llm_env + 0xc4) * 100,*(code **)(_r_modules_funcs_p + 0xc4))
     ;
-    (**(code **)(_r_ip_funcs_p + 0x294))(*(code **)(_r_ip_funcs_p + 0x294));
   }
+  (**(code **)(_r_ip_funcs_p + 0x294))(*(code **)(_r_ip_funcs_p + 0x294));
   return 0;
 }
 
